297e8f7c43f9773769e744b6d7e60378c99da443
bast478 <bast0ch@protonmail.com>
2022-08-24 10:47:55 +0200

    Amélioration du script, clique à côté de la carte la ferme.

--- script.js
+++ script.js
@@ -5,69 +5,51 @@ function setAttributes(el, attrs) {
     }
 }
 
-//modalIsOpen, modalContentImgs, menuGridCards, this, e, subMenuGridCards, linkPictures, linkGithub, targetGridCards
-
-function quitMenuGridCardsClickWindow(this) {
-    console.log(this);
-    // if (modalIsOpen === 1) {
-    //             for (let img of modalContentImgs) {
-    //                 if (e.target !== img) {
-    //                     //clickWindow ++;
-    //                     //clickCard ++;
-    //                     menuGridCards.replaceWith(this);
-    //                 }
-    //             }
-    //         } else if (e.target !== subMenuGridCards && e.target !== menuGridCards && e.target !== linkPictures && e.target !== linkGithub && e.target !== targetGridCards) {
-    //             //clickWindow ++;
-    //             //clickCard ++;
-    //             menuGridCards.replaceWith(this);
-    //         }
-}
-
 // VARIABLES
 const svgNS = "http://www.w3.org/2000/svg";
 const gridCards = document.querySelectorAll('.grid-card');
-
-let oldCard;
-let oldMenuGridCards;
+/*croix*/
+let cross = document.createElementNS(svgNS, "svg");
+setAttributes(cross, {"class": "cross", "xmlns": "http://www.w3.org/2000/svg", "viewBox": "0 0 13.6 13.6", "width": "13.6", "height": "13.6"});
+let path = document.createElementNS(svgNS, "path");
+setAttributes(path, {"class": "path", "d": "M 13.6,1.01 12.59,0 6.8,5.79 1.01,0 0,1.01 5.79,6.8 0,12.59 1.01,13.6 6.8,7.81 12.59,13.6 13.6,12.59 7.81,6.8 Z"});
+cross.appendChild(path);
+/*fermer la carte précedente quand on clique sur la nouvelle*/
+let prevCard;
+let prevMenuGridCards;
+/*quitter les réalisations au click*/
+let modalIsOpen = 0;
+let modalContentImgs, subMenuGridCards, linkPictures, linkGithub, targetGridCards;
 
 gridCards.forEach(item => {
     item.addEventListener('click', function(e) {
         e.preventDefault();
-        if (oldMenuGridCards !== undefined) {
-            oldMenuGridCards.replaceWith(oldCard);
+        if (prevMenuGridCards !== undefined) {
+            prevMenuGridCards.replaceWith(prevCard);
         }
-        oldCard = this;
+        prevCard = this;
         // conteneur
-        let menuGridCards = document.createElement('div');
-        oldMenuGridCards = menuGridCards;
+        menuGridCards = document.createElement('div');
+        prevMenuGridCards = menuGridCards;
         menuGridCards.classList.add('menu-grid-cards');
         this.parentNode.insertBefore(menuGridCards, this);
         menuGridCards.appendChild(this);
         // sous conteneur
-        let subMenuGridCards = document.createElement('div');
+        subMenuGridCards = document.createElement('div');
         subMenuGridCards.classList.add('sub-menu-grid-cards');
-        // croix
-        let cross = document.createElementNS(svgNS, "svg");
-        setAttributes(cross, {"class": "cross", "xmlns": "http://www.w3.org/2000/svg", "viewBox": "0 0 13.6 13.6", "width": "13.6", "height": "13.6"});
-        let path = document.createElementNS(svgNS, "path");
-        setAttributes(path, {"class": "path", "d": "M 13.6,1.01 12.59,0 6.8,5.79 1.01,0 0,1.01 5.79,6.8 0,12.59 1.01,13.6 6.8,7.81 12.59,13.6 13.6,12.59 7.81,6.8 Z"});
-        cross.appendChild(path);
         // images
-        let linkPictures = document.createElement('a');
+        linkPictures = document.createElement('a');
         linkPictures.href = '#';
         linkPictures.textContent = 'Images';
         // github
-        let linkGithub = document.createElement('a');
+        linkGithub = document.createElement('a');
         linkGithub.href = this.href;
         linkGithub.target = '_blank';
         linkGithub.textContent = 'Github';
         subMenuGridCards.append(cross, linkPictures, linkGithub);
         menuGridCards.appendChild(subMenuGridCards);
-
         // open modal
-        let modalIsOpen = 0;
-        const modalContentImgs = [];
+        modalContentImgs = [];
 
         const modal = document.createElement('div');
         modal.classList.add('modal-pic-grid-card');
@@ -78,7 +60,6 @@ gridCards.forEach(item => {
         linkPictures.onclick = (e) => {
             e.preventDefault()
             modalIsOpen = 1;
-            console.log("modalIsOpen = " + modalIsOpen);
             menuGridCards.appendChild(modal);
             fetch('site-pictures.json')
             .then (response => response.json())
@@ -98,7 +79,7 @@ gridCards.forEach(item => {
         // fermer le menu
         //let clickCard = 0;
         //let clickWindow = 0;
-        const targetGridCards = e.target;
+        targetGridCards = e.target;
         // cross.addEventListener('click', () => {
         //     console.log("dfd");
         //     menuGridCards.replaceWith(this);
@@ -129,9 +110,20 @@ gridCards.forEach(item => {
         //         menuGridCards.replaceWith(this);
         //     }
         // };
-
-        window.addEventListener('click', quitMenuGridCardsClickWindow(menuGridCards), {capture: true});
-
-
     });
+});
+
+window.addEventListener('click', function (e) {
+    if (modalIsOpen === 1) {
+        for (let img of modalContentImgs) {
+            if (e.target !== img) {
+                prevMenuGridCards.replaceWith(prevCard);
+                modalIsOpen = 0;
+                prevMenuGridCards = undefined;
+            }
+        }
+    } else if (prevMenuGridCards !== undefined && e.target !== targetGridCards && e.target !== subMenuGridCards && e.target !== prevMenuGridCards && e.target !== linkPictures && e.target !== linkGithub) {
+         prevMenuGridCards.replaceWith(prevCard);
+         prevMenuGridCards = undefined;
+    }
 });
